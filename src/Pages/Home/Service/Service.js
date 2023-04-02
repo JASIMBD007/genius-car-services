03c249b35bba635cb1dbc0678348756d3e05a6a7
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-04-02 13:51:56 +0200

    more components added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Service.css';
 
 const Service = ({ service }) => {
     const { name, img, description, price } = service;
     return (
-        <div>
+        <div className='service'>
             <img src={img} alt="" srcset="" />
             <h2>{name}</h2>
+            <p>{description}</p>
             <p>Price:{price}</p>
             <button>Book : {name}</button>
         </div>
