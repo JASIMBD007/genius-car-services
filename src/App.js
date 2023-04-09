25740ed50239ae2d65808afd86b9d4afda9536c6
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-04-09 16:12:15 +0200

    dynamic route added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 
 function App () {
   return (
@@ -12,6 +13,7 @@ function App () {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/service/:serviceId' element={<ServiceDetail />} />
         <Route path='/about' element={<About />} />
       </Routes>
       <Footer></Footer>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Service.css';
+import { useNavigate } from 'react-router-dom';
 
 const Service = ({ service }) => {
-    const { name, img, description, price } = service;
+    const { id, name, img, description, price } = service;
+    const navigate = useNavigate();
+    const navigateToServiceDetail = id => {
+        navigate(`/service/${id}`);
+    }
     return (
         <div className='service'>
             <img className='w-100' src={img} alt="" />
             <h2>{name}</h2>
             <p>{description}</p>
             <p>Price:{price}</p>
-            <button className='btn btn-primary'>BOOK : {name}</button>
+            <button onClick={() => navigateToServiceDetail(id)} className='btn btn-primary'>BOOK : {name}</button>
         </div>
     );
 };
