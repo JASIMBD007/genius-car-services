c7b5b5a8fb59dfea8abc0fde8439f354c1fe34a2
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-04-10 14:16:10 +0200

    login form added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -3,13 +3,22 @@ import { Button, Form } from 'react-bootstrap';
 
 const Login = () => {
     const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(email, password);
+    }
     return (
         <div className='container w-50 mx-auto'>
             <h2 className='text-primary text-center mt-2 '>Please Login</h2>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -17,15 +26,16 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
-                    Submit
+                    Login
                 </Button>
             </Form>
+            <p>New to Car Doctor <span className='text-danger'>Please Register</span> </p>
         </div>
     );
 };
