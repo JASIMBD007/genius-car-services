b8e4debc843c13f82f039cbff3dc9bb5cd3ee9ce
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-04-10 13:55:22 +0200

    login and register component created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
+import NotFound from './Pages/Shared/NotFound/NotFound';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 function App () {
   return (
@@ -13,8 +16,12 @@ function App () {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/service/:serviceId' element={<ServiceDetail />} />
         <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
       <Footer></Footer>
     </div>
