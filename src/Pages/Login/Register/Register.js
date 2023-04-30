f1ed43d91106fd15a3d7d3056c3a5be7c5329145
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-04-30 14:26:41 +0200

    more social btn added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,6 +3,7 @@ import './Register.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 
 const Register = () => {
@@ -27,7 +28,7 @@ const Register = () => {
     }
     return (
         <div className='register-form'>
-            <h2 style={{ textAlign: 'center' }}>Please Register</h2>
+            <h2 style={{ textAlign: 'center' }} className='text-primary'>Please Register</h2>
             <form onSubmit={handleRegister} >
                 <input type="text" name="name" id="" placeholder='Your Name' />
                 <input type="email" name="email" id="" placeholder='Your Email' required />
@@ -35,6 +36,7 @@ const Register = () => {
                 <input type="submit" value="Register" />
             </form>
             <p>Already have an account ? <Link to='/login' className='text-danger pe-auto text-decoration-none ' onClick={navigateLogin}>Please Login</Link> </p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import google from '../../../images/social/google.png';
+import facebook from '../../../images/social/facebook.png';
+import github from '../../../images/social/github.png';
+
 
 const SocialLogin = () => {
     return (
-        <div >
+        <div>
             <div className='d-flex align-items-center'>
                 <div style={{ height: '1px' }} className='bg-primary w-50'></div>
                 <p className='mt-2 px-2'>Or</p>
                 <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+            </div>
+            <div>
+                <button className='btn btn-primary w-50 d-block mx-auto my-2'> <img style={{ width: "30px " }} src={google} alt="" srcset="" /> <span className='px-2'>Sign In With Google </span> </button>
+                <button className='btn btn-primary w-50 d-block mx-auto my-2'> <img style={{ width: "30px " }} src={facebook} alt="" srcset="" /> <span className='px-2'>Sign In With Facebook </span> </button>
+                <button className='btn btn-primary w-50 d-block mx-auto my-2'> <img style={{ width: "30px " }} src={github} alt="" srcset="" /> <span className='px-2'>Sign In With Github </span> </button>
             </div>
         </div>
     );
