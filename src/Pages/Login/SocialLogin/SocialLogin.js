96208bfffc5e54ad9255c266c7aaffefa23d8678
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-04-30 16:27:57 +0200

    google sign in added

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,9 +2,27 @@ import React from 'react';
 import google from '../../../images/social/google.png';
 import facebook from '../../../images/social/facebook.png';
 import github from '../../../images/social/github.png';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
 
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+    const navigate = useNavigate();
+    let errorElement = '';
+
+    if (error) {
+        errorElement = <div>
+            <p className='text-danger'>Error: {error.message}</p>
+        </div>
+
+    }
+
+    if (user) {
+        navigate('/home');
+    }
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -12,8 +30,9 @@ const SocialLogin = () => {
                 <p className='mt-2 px-2'>Or</p>
                 <div style={{ height: '1px' }} className='bg-primary w-50'></div>
             </div>
+            {errorElement}
             <div>
-                <button className='btn btn-primary w-50 d-block mx-auto my-2'> <img style={{ width: "30px " }} src={google} alt="" srcset="" /> <span className='px-2'>Sign In With Google </span> </button>
+                <button onClick={() => signInWithGoogle()} className='btn btn-primary w-50 d-block mx-auto my-2'> <img style={{ width: "30px " }} src={google} alt="" srcset="" /> <span className='px-2'>Sign In With Google </span> </button>
                 <button className='btn btn-primary w-50 d-block mx-auto my-2'> <img style={{ width: "30px " }} src={facebook} alt="" srcset="" /> <span className='px-2'>Sign In With Facebook </span> </button>
                 <button className='btn btn-primary w-50 d-block mx-auto my-2'> <img style={{ width: "30px " }} src={github} alt="" srcset="" /> <span className='px-2'>Sign In With Github </span> </button>
             </div>
