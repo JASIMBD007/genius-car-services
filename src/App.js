ea224df33f3e6b797cd270df2f5da743e74ec27b
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-07-10 21:15:58 +0200

    checkout updated with axios

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App () {
         <Route path='/about' element={<About />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/checkout' element={
+        <Route path='/checkout/:serviceId' element={
           <ProtectedRoute>
             <Checkout />
           </ProtectedRoute>
--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import useServiceDetail from '../../../hooks/useServiceDetail';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import axios from 'axios';
 
 const Checkout = () => {
+    const { serviceId } = useParams();
+    const [service] = useServiceDetail(serviceId);
+    const [user] = useAuthState(auth);
+    // const [user, setUser] = useState({
+    //     Name: 'Jasim Uddin',
+    //     email: 'jasim.ice.iu@gmail.com',
+    //     address: 'Huttrop Str.46',
+    //     phone: '01629232088'
+    // });
+
+    // const handleAddressChange = event => {
+    //     console.log(event.target.value);
+    //     const { address, ...rest } = user;
+    //     const newAddress = event.target.value;
+    //     const newUser = { address: newAddress, ...rest };
+    //     console.log(newUser);
+    //     setUser(newUser);
+    // }
+
+    const handlePlaceOrder = event => {
+        event.preventDefault();
+        const order = {
+            email: user.email,
+            service: service.name,
+            serviceId: serviceId,
+            address: event.target.address.value,
+            phone: event.target.phone.value
+        }
+        axios.post('http://localhost:5000/order', order)
+            .then(response => {
+                console.log(response);
+            })
+
+    }
     return (
-        <div>
-            <h2>Checkout Your Bookings </h2>
+        <div className='w-50 mx-auto'>
+            <h2>Please Order : {service.name}</h2>
+            <form onSubmit={handlePlaceOrder}>
+                <input className='w-100 mb-2' type="text" value={user.displayName} name="name" placeholder='Name' required readOnly />
+                <br />
+                <input className='w-100 mb-2' type="email" value={user.email} name="email" placeholder='Email' required readOnly disabled />
+                <br />
+                <input className='w-100 mb-2' type="text" value={service.name} name="service" placeholder='Service' required readOnly />
+                <br />
+                <input className='w-100 mb-2' type="text" name="address" placeholder='Address' required autoComplete='off' />
+                <br />
+                <input className='w-100 mb-2' type="text" value={user.phone} name="phone" placeholder='Phone' required />
+                <br />
+                <input className='btn btn-primary' type="submit" value="Place Order" />
+            </form>
         </div>
     );
 };
